0be591e818ae973e41c9f7da935bc7f1065c22a4
Ben Schwartz <benschw@gmail.com>
2015-09-24 20:11:00 -0500

    update to mitchelh goamz

--- main.go
+++ main.go
@@ -1,47 +1,59 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
 )
 
 func main() {
-
-	// DO NOT PUT credentials in code for production usage!
-	// see https://www.socketloop.com/tutorials/golang-setting-up-configure-aws-credentials-with-official-aws-sdk-go
-	// on setting creds from environment or loading from file
-
-	// the file location and load default profile
-	creds := credentials.NewEnvCredentials()
-
-	_, err := creds.Get()
-
+	auth, err := aws.EnvAuth()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
+	}
+	client := s3.New(auth, aws.USEast)
+
+	resp, err := client.ListBuckets()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.Print(fmt.Sprintf("%T %+v", resp.Buckets[0], resp.Buckets[0]))
+
+	bucket := client.Bucket("fliglio")
+
+	//data := []byte("Hello World")
+	data, fileType, path, err := foo("test.txt")
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 
-	config := aws.NewConfig().WithRegion("us-east-1").WithEndpoint("s3.amazonaws.com").WithCredentials(creds)
+	err = bucket.Put(path, data, fileType, s3.BucketOwnerFull)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-	s3client := s3.New(config)
+	out, err := bucket.Get(path)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.Printf("%s", out)
 
-	bucketName := "fliglio" // <-- change this to your bucket name
+}
 
-	fileToUpload := "test.txt"
+func foo(fileToUpload string) ([]byte, string, string, error) {
 
 	file, err := os.Open(fileToUpload)
 
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil, "", "", err
 	}
 
 	defer file.Close()
@@ -50,38 +62,9 @@ func main() {
 	var size int64 = fileInfo.Size()
 
 	buffer := make([]byte, size)
-	// read file content to buffer
 	file.Read(buffer)
-
-	fileBytes := bytes.NewReader(buffer) // convert to io.ReadSeeker type
 
 	fileType := http.DetectContentType(buffer)
 
-	path := "/examplefolder/" + file.Name() // target file and location in S3
-
-	params := &s3.PutObjectInput{
-		Bucket:        aws.String(bucketName), // required
-		Key:           aws.String(path),       // required
-		Body:          fileBytes,
-		ContentLength: &size,
-		ContentType:   aws.String(fileType),
-		Metadata: map[string]*string{
-			"Key": aws.String("MetadataValue"), //required
-		},
-		// see more at http://godoc.org/github.com/aws/aws-sdk-go/service/s3#S3.PutObject
-	}
-
-	log.Println("here")
-	result, err := s3client.PutObject(params)
-	log.Println("here")
-
-	if err != nil {
-		if awsErr, ok := err.(awserr.Error); ok {
-			log.Printf("%+v", awsErr)
-		} else {
-			log.Printf("%+v", err)
-		}
-	}
-
-	fmt.Printf("%+v", result)
+	return buffer, fileType, file.Name(), nil
 }
